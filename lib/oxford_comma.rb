b77b22366760056869322c2c779702a698b29965
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-26 21:44:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length==1
     array.join
-  else array.length==2
+  elseif array.length==2
     array.join(" and ")
   else array.length >= 3
     array.join(", ")
